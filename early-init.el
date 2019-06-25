e0cd6b9af7e5dfc56d7b90204bb5ac62f1449142
Quentin LE GUENNEC <quentin.le-guennec@ratp.fr>
2019-06-25 09:49:35 +0200

    Fix gc treshold

--- early-init.el
+++ early-init.el
@@ -6,7 +6,7 @@
 ;; before package and UI initialization happens.
 
 ;; Defer garbage collection further back in the startup process
-(setq gc-cons-threshold 1073741824)
+(setq gc-cons-threshold 536870912)
 
 ;; Package initialize occurs automatically, before `user-init-file' is
 ;; loaded, but after `early-init-file'. Doom handles package
