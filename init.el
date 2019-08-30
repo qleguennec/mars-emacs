35de3e147b8b90fb5de3ebedeb4412baa90f5a3b
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2019-08-30 21:41:53 +0200

    Fix ivy-rich

--- init.el
+++ init.el
@@ -516,74 +516,11 @@ If point is on a src block, runs org-indent"
     :config (ivy-prescient-mode 1))
 
   (use-package ivy-rich
-    :after (ivy counsel)
+    :after (counsel ivy)
     :demand t
-    :init
-    (setq ivy-rich-path-style 'abbrev
-	  ivy-virtual-abbreviate 'full
-	  ivy-format-function #'ivy-format-function-line)
-
     :config
-    (setq ivy-format-function #'ivy-format-function-line)
-
-    (defun ivy-rich-switch-buffer-icon (candidate)
-      (with-current-buffer
-   	  (get-buffer candidate)
-	(let ((icon (all-the-icons-icon-for-mode major-mode)))
-	  (if (symbolp icon)
-	      (all-the-icons-icon-for-mode 'fundamental-mode)
-	    icon))))
-
-    (setq ivy-rich-display-transformers-list
-	  '(ivy-switch-buffer
-	    (:columns
-	     ((ivy-rich-switch-buffer-icon :width 2)
-	      (ivy-rich-candidate
-	       (:width 30))
-	      (ivy-rich-switch-buffer-size
-	       (:width 7))
-	      (ivy-rich-switch-buffer-indicators
-	       (:width 4 :face error :align right))
-	      (ivy-rich-switch-buffer-major-mode
-	       (:width 12 :face warning))
-	      (ivy-rich-switch-buffer-project
-	       (:width 15 :face success))
-	      (ivy-rich-switch-buffer-path
-	       (:width
-		(lambda
-		  (x)
-		  (ivy-rich-switch-buffer-shorten-path x
-						       (ivy-rich-minibuffer-width 0.3))))))
-	     :predicate
-	     (lambda (cand)
-	       (get-buffer cand)))
-	    counsel-M-x
-	    (:columns
-	     ((counsel-M-x-transformer
-	       (:width 40))
-	      (ivy-rich-counsel-function-docstring
-	       (:face font-lock-doc-face))))
-	    counsel-describe-function
-	    (:columns
-	     ((counsel-describe-function-transformer
-	       (:width 40))
-	      (ivy-rich-counsel-function-docstring
-	       (:face font-lock-doc-face))))
-	    counsel-describe-variable
-	    (:columns
-	     ((counsel-describe-variable-transformer
-	       (:width 40))
-	      (ivy-rich-counsel-variable-docstring
-	       (:face font-lock-doc-face))))
-	    counsel-recentf
-	    (:columns
-	     ((ivy-rich-candidate
-	       (:width 0.8))
-	      (ivy-rich-file-last-modified-time
-	       (:face font-lock-comment-face))))))
-
-    (ivy-rich-mode))
-
+    (ivy-rich-mode)
+    (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line))
 
   (use-package wgrep)
 
